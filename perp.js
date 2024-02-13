0ed0bea40d180898ed1559f0a02caaa46f8eec95
selfint <selfint@gmail.com>
2024-02-13 22:52:56 +0200

    show progress

--- perp.js
+++ perp.js
@@ -16,25 +16,12 @@ function mean(numbers) {
 }
 
 const bad = [
-    `def fib(n):
-    `,
-    `if n == 0:
-        return 0
-
-    if n <= 1:
-        return 1
-
-    return n + fibonacci(n-1, n-2)
-`
-];
-
-const good = [
-    `def fibonacci(n: int):
+    `def fibonacci(n: int) -> int:
     """
     Calculate the n-th Fibonacci number.
 
     Args:
-        n (int) - nth number to calculate
+        n (int) - nth number to calculate.
 
     Returns:
         int - the nth fibonacci number.
@@ -44,44 +31,35 @@ const good = [
     assert n >= 0, f"Invalid value {n=!r} < 0"
 
     if n <= 1:
-        return 1
+        return n
 
     return n + fibonacci(n-1, n-2)`
 ];
 
-/**
-import torch
-from tqdm import tqdm
+const good = [
+    `def fibonacci(n: int) -> int:
+    """
+    Calculate the n-th Fibonacci number.
 
-max_length = model.config.n_positions
-stride = 512
-seq_len = encodings.input_ids.size(1)
+    Args:
+        n (int) - nth number to calculate.
 
-nlls = []
-prev_end_loc = 0
-for begin_loc in tqdm(range(0, seq_len, stride)):
-    end_loc = min(begin_loc + max_length, seq_len)
-    trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
-    input_ids = encodings.input_ids[:, begin_loc:end_loc].to(device)
-    target_ids = input_ids.clone()
-    target_ids[:, :-trg_len] = -100
+    Raises:
+        AssertionError - if n < 0.
 
-    with torch.no_grad():
-        outputs = model(input_ids, labels=target_ids)
+    Returns:
+        int - the nth fibonacci number.
+    """
+    `,
+    `
+    assert n >= 0, f"Invalid value {n=!r} < 0"
 
-        # loss is calculated using CrossEntropyLoss which averages over valid labels
-        # N.B. the model only calculates loss over trg_len - 1 labels, because it internally shifts the labels
-        # to the left by 1.
-        neg_log_likelihood = outputs.loss
+    if n <= 1:
+        return n
 
-    nlls.append(neg_log_likelihood)
+    return n + fibonacci(n-1, n-2)`
+];
 
-    prev_end_loc = end_loc
-    if end_loc == seq_len:
-        break
-
-ppl = torch.exp(torch.stack(nlls).mean())
- */
 
 async function calculatePerplexity(pipe, [signature, implementation]) {
     const model = pipe.model;
@@ -92,12 +70,14 @@ async function calculatePerplexity(pipe, [signature, implementation]) {
 
     const scores = [];
     for (let i = 1; i < impTokens.length - 1; i++) {
+        /** @type {string} */
         const token = tokenizer.decode([impTokens[i]]);
         const context = tokenizer(tokenizer.decode(sigTokens.concat(impTokens.slice(0, i))));
         const target = Number(impTokens[i]);
         // this is wrong
+        /** @type {number} */
         const perplexity = softmax((await model(context)).logits[0][-1].data)[target];
-        console.log(`${i}/${impTokens.length}: '${token}' ${perplexity}`);
+        console.log(`${i.toString().padStart(3, ' ')}/${impTokens.length} = ${perplexity.toString().padStart(18, ' ')} '${token}'`);
         scores.push(perplexity)
     }
 
